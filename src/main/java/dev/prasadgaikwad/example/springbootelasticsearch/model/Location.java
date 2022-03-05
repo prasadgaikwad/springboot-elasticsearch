245cf66362a03c86055263f08f6b868704e2f86e
Prasad Gaikwad <prasad.gaikwad@gmail.com>
2022-03-04 21:19:45 -0600

    added field names

--- src/main/java/dev/prasadgaikwad/example/springbootelasticsearch/model/Location.java
+++ src/main/java/dev/prasadgaikwad/example/springbootelasticsearch/model/Location.java
@@ -6,6 +6,7 @@ import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 import java.time.Instant;
 
@@ -18,17 +19,23 @@ public class Location extends BaseModel {
     private String id;
     private String title;
     @JsonProperty(value = "location_type")
+    @Field(value = "location_type")
     private String locationType;
     @JsonProperty(value = "woeid")
+    @Field(value = "woeid")
     private String woeId;
     @JsonProperty(value = "latt_long")
+    @Field(value = "latt_long")
     private String lattLong;
     private String timezone;
     @JsonProperty(value = "timezone_name")
+    @Field(value = "timezone_name")
     private String timezoneName;
     @JsonProperty(value = "sun_set")
+    @Field(value = "sun_set")
     private Instant sunSet;
     @JsonProperty(value = "sun_rise")
+    @Field(value = "sun_rise")
     private Instant sunRise;
     private Instant time;
     private Location parent;
